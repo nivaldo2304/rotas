2fb470d9d896fec1f8fe41624b3828b3bda8db3b
Nivaldo Rodrigues <nivaldo_rodrigues@hotmail.com.br>
2023-03-20 15:15:53 -0300

    add route project

---
+++ src/app/pages/portifolio/card/card.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit{
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private navegador:Router
+    ){
+    //http://localhost:4200/portfolio/{1}
+    this.activeRoute.params.subscribe(
+      res => console.log(res)
+    )
+
+    this.activeRoute.firstChild?.params.subscribe(
+      res => console.log(res)
+    )
+
+    //http://localhost:4200/portfolio/1?name=nivaldo&token=123
+    this.activeRoute.queryParams.subscribe(
+      res => console.log(res)
+    )
+  }
+
+  ngOnInit(): void {
+    //setInterval(()=>{
+    //  this.navegador.navigate(['/'])
+    //}, 5000)
+  }
+
+}
